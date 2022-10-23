15d127057b0674ef949249383c104f827f62b6b0
Waoweens <30313089+Waoweens@users.noreply.github.com>
2022-10-24 06:35:43 +0700

    show jvm args on window

--- src/main/java/net/meowcorp/smp/bootstrap/Main.java
+++ src/main/java/net/meowcorp/smp/bootstrap/Main.java
@@ -1,12 +1,19 @@
 package net.meowcorp.smp.bootstrap;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Picked up -DminecraftInstance=");
-        System.out.println(System.getProperty("minecraftInstance"));
         if (System.getProperty("minecraftInstance").equals("smpCompatible.instance")) {
             System.out.println("Detected compatible instance!");
-            MainWindow mainWindow = new MainWindow();
+
+            RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+            List<String> jvmArgs = bean.getInputArguments();
+
+            MainWindow mainWindow = new MainWindow(jvmArgs);
         } else {
             System.out.println("Detected incompatible instance!");
             InvalidWindow invalidWindow = new InvalidWindow();
--- src/main/java/net/meowcorp/smp/bootstrap/MainWindow.java
+++ src/main/java/net/meowcorp/smp/bootstrap/MainWindow.java
@@ -1,12 +1,22 @@
 package net.meowcorp.smp.bootstrap;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.List;
 
 public class MainWindow extends JFrame {
-    public MainWindow() {
+    List<String> jvmArgs;
+    public MainWindow(List<String> jvmArgs) {
         this.setTitle("SMP Bootstrapper");
 
+        JPanel panelArgs = new JPanel();
+        panelArgs.setLayout(new FlowLayout());
 
+        for (String jvmArg : jvmArgs) {
+            panelArgs.add(new JLabel(jvmArg));
+        }
+
+        this.add(panelArgs);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,250);
         this.setVisible(true);
